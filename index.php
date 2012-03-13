21d8184fc50138aa138409d4d904d6e17dbf15c5
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-14 00:54:13 +0200

    database init

--- index.php
+++ index.php
@@ -5,10 +5,15 @@
 <h2>
 <?php
     include 'connect.php';
+    include 'database.php';
     connect();
+    query_array('SELECT *
+        FROM `AIRCRAFT_MODEL`
+        LIMIT 0 , 30'
+        );
     disconnect();
     
-	echo "Hello World";
+	echo "<p>Hello World</p>";
 ?>
 </h2>
 </body>
