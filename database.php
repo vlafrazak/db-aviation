08fe1118d10365171a7d2b4abcb01e6d427484fc
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-14 23:22:52 +0200

    changes file structure

--- database.php
+++ database.php
@@ -4,16 +4,23 @@ function query_array($query) {
     $result = mysql_query($query) or die(mysql_error());
 
     Print "<table border cellpadding=3>"; 
-    while($row = mysql_fetch_array( $result )) 
-    { 
+    while($row = mysql_fetch_array( $result )) { 
         Print "<tr>"; 
+        Print "<th>Emp_Code:</th> <td>".$row['Emp_Code'] . "</td> "; 
         Print "<th>SSN:</th> <td>".$row['SSN'] . "</td> "; 
-        Print "<th>Surname:</th> <td>".$row['Surname'] . " </td></tr>"; 
+        Print "<th>Union_Membership_Number:</th> <td>".$row['Union_Membership_Number'] . "</td> "; 
+        Print "<th>Surname:</th> <td>".$row['Surname'] . " </td>"; 
+        Print "<th>Street_Name:</th> <td>".$row['Street_Name'] . "</td> "; 
+        Print "<th>Street_Number:</th> <td>".$row['Street_Number'] . "</td> "; 
+        Print "<th>Postal_Code:</th> <td>".$row['Postal_Code'] . "</td> "; 
+        Print "<th>Year_of_Birth:</th> <td>".$row['Year_of_Birth'] . "</td> "; 
+        Print "<th>Salary:</th> <td>".$row['Salary'] . "</td> </tr>"; 
     } 
     Print "</table>"; 
     mysql_free_result($result);
 }
 
+function insert
 mysql_query("INSERT INTO `aviation`.`EMPLOYEES` (
     `Emp_Code` ,
     `SSN` ,
@@ -30,41 +37,6 @@ VALUES (
 )");
 
 
-function query_ins($query) {
-    $sql=("INSERT INTO EMPLOYEES (
-        `Emp_Code`,
-        `SSN`,
-        `Union_Membership_Number`,
-        `First_Name`,
-        `Surname`,
-        `Street_Name`,
-        `Street_Number`,
-        `Postal_Code`,
-        `Year_of_Birth`,
-        `Salary`
-    )
-    VALUES(
-        '$_POST[Emp_Code]',
-        '$_POST[SSN]',
-        '$_POST[Union_Membership_Number]',
-        '$_POST[First_Name]', 
-        '$_POST[Surname]',
-        '$_POST[Street_Name]',
-        '$_POST[Street_Number]',
-        '$_POST[Postal_Code]',
-        '$_POST[Year_of_Birth]',
-        '$_POST[Salary]'
-    )");
-
-if (!mysql_query($sql,$con))
-      {
-            die('Error: ' . mysql_error());
-              }
-echo "1 record added";
-
-//    echo "$row[1]<br>";  
-//    echo "$row[2]<br>";  
-}
 
 mysql_free_result($result);
 ?>
--- index.php
+++ index.php
@@ -1,42 +1,15 @@
-<html>
-<body>
-<h1>Test</h1>
-
 
 <?php
-    include 'connect.php';
-    include 'database.php';
+    include 'header.php';
+?>
 
-    connect();
-    query_array('SELECT *
-        FROM EMPLOYEES'
-    );
+<h1>Test</h1>
 
-       "SELECT *  FROM TECHNICIANS";
-
-    $result=mysql_query($query);
-
-    while($row=mysql_fetch_array($result))
-
-    {
-
-            print("$row[roll_no],$row[name],$row[specialization]");
-
-            }
-   
-       
-    while($data=mysql_fetch_row($result))
-       
-    {
-       
-           print("$data[0],$data[1],$data[2],$data[3]");
-          
-    } 
-    
-    disconnect();
-    
+<?php
 	echo "<p>Hello World</p>";
 ?>
 
-</body>
-</html>
+<?php
+    include 'footer.php';
+?>
+
