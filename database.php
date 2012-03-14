9c38e42607cf29d1d10399f4e3e85d87064b3415
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-14 22:23:05 +0200

    employees insert

--- database.php
+++ database.php
@@ -1,10 +1,70 @@
 <?php
-function query_array($query){
-$result = mysql_query($query);
+function query_array($query) {
+    Print "query_array<br>";    
+    $result = mysql_query($query) or die(mysql_error());
 
-while ($row = mysql_fetch_array($result)) {
-    echo $row[1];  
+    Print "<table border cellpadding=3>"; 
+    while($row = mysql_fetch_array( $result )) 
+    { 
+        Print "<tr>"; 
+        Print "<th>SSN:</th> <td>".$row['SSN'] . "</td> "; 
+        Print "<th>Surname:</th> <td>".$row['Surname'] . " </td></tr>"; 
+    } 
+    Print "</table>"; 
+    mysql_free_result($result);
 }
+
+mysql_query("INSERT INTO `aviation`.`EMPLOYEES` (
+    `Emp_Code` ,
+    `SSN` ,
+    `First_Name` ,
+    `Surname` ,
+    `Street_Name` ,
+    `Street_Number` ,
+    `Postal_Code` ,
+    `Year_of_Birth` ,
+    `Salary`
+)
+VALUES (
+    '03108137', '333', 'Iren', 'Vlassi', 'Petridou', '23', '77305', '1968', '40020'
+)");
+
+
+function query_ins($query) {
+    $sql=("INSERT INTO EMPLOYEES (
+        `Emp_Code`,
+        `SSN`,
+        `Union_Membership_Number`,
+        `First_Name`,
+        `Surname`,
+        `Street_Name`,
+        `Street_Number`,
+        `Postal_Code`,
+        `Year_of_Birth`,
+        `Salary`
+    )
+    VALUES(
+        '$_POST[Emp_Code]',
+        '$_POST[SSN]',
+        '$_POST[Union_Membership_Number]',
+        '$_POST[First_Name]', 
+        '$_POST[Surname]',
+        '$_POST[Street_Name]',
+        '$_POST[Street_Number]',
+        '$_POST[Postal_Code]',
+        '$_POST[Year_of_Birth]',
+        '$_POST[Salary]'
+    )");
+
+if (!mysql_query($sql,$con))
+      {
+            die('Error: ' . mysql_error());
+              }
+echo "1 record added";
+
+//    echo "$row[1]<br>";  
+//    echo "$row[2]<br>";  
 }
+
 mysql_free_result($result);
 ?>
--- index.php
+++ index.php
@@ -2,19 +2,41 @@
 <body>
 <h1>Test</h1>
 
-<h2>
+
 <?php
     include 'connect.php';
     include 'database.php';
+
     connect();
     query_array('SELECT *
-        FROM `AIRCRAFT_MODEL`
-        LIMIT 0 , 30'
-        );
+        FROM EMPLOYEES'
+    );
+
+       "SELECT *  FROM TECHNICIANS";
+
+    $result=mysql_query($query);
+
+    while($row=mysql_fetch_array($result))
+
+    {
+
+            print("$row[roll_no],$row[name],$row[specialization]");
+
+            }
+   
+       
+    while($data=mysql_fetch_row($result))
+       
+    {
+       
+           print("$data[0],$data[1],$data[2],$data[3]");
+          
+    } 
+    
     disconnect();
     
 	echo "<p>Hello World</p>";
 ?>
-</h2>
+
 </body>
 </html>
