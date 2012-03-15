ab1e7fbc065224acd617c977d95cbbf982ff3205
iren <iren.vlsi@gmai.com>
2012-03-15 14:13:20 +0200

    aircraft fleet

---
+++ views/listAircraftFleet.php
@@ -0,0 +1,30 @@
+<?php
+    include "../header.php";
+?>
+
+
+<table border = "1" >
+<tr>
+<th>Registration_Number</th> 
+<th>Air_Carrier</th> 
+<th>Construction_Date</th> 
+<th>Model_Code</th> 
+</tr>
+
+<?php
+    $employees = include "../models/listEmployee.php";
+
+    foreach( $employees as $employee ) {
+        echo "<tr>";
+        echo "<td>" .  $employee[ 'Registration_Number' ] . "</td>";
+        echo "<td>" .  $employee[ 'Air_Carrier' ] . "</td>";
+        echo "<td>" .  $employee[ 'Construction_Date' ] . "</td>";
+        echo "<td>" .  $employee[ 'Model_Code' ] . "</td>";
+        echo "</tr>";
+    }
+
+?>
+</table>
+<?php
+    include "../footer.php";
+?>
