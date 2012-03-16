e97582c71bc44fee1c45b7f20d346333c6c58c82
Gregory Liras <gregliras@gmail.com>
2012-03-16 23:19:41 +0200

    left join it is

--- models/listEmployees.php
+++ models/listEmployees.php
@@ -2,18 +2,14 @@
     include 'database.php';
 
     $employees = query_array( 
-        "SELECT
-            e . * , m.E_mail, p.Phone_Number
-        FROM 
-            EMPLOYEES e
-        INNER JOIN 
-            E_MAILS m
-        INNER JOIN 
-            PHONES p 
-            ON (
-                e.Emp_Code = p.Emp_Code
-                AND e.Emp_Code = p.Emp_Code
-            )"
+        "SELECT e . * , m.E_mail, p.Phone_Number
+        FROM EMPLOYEES e
+        LEFT JOIN (
+            E_MAILS m, PHONES p
+        ) ON (
+            e.Emp_Code = p.Emp_Code
+            AND e.Emp_Code = p.Emp_Code
+        )"
         );
     if( !$employees ) {
         die( mysql_error() );
