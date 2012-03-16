a55fe194ab12cbae7af098786e24381dbea36cd5
Gregory Liras <gregliras@gmail.com>
2012-03-17 01:09:15 +0200

    AirTrafficControllers, no phone join...

--- models/listAirTrafficContrllers.php
+++ models/listAirTrafficContrllers.php
@@ -3,20 +3,14 @@
 
     $controllers = query_array( 
         "SELECT 
-            e . * , m.E_mail, p.Phone_Number, atc.Last_Exam_Date, atc.Exam_Result
+            e . * , atc.Last_Exam_Date, atc.Exam_Result
         FROM 
             EMPLOYEES e
-        LEFT JOIN 
-            E_MAILS m
-        LEFT JOIN 
-            PHONES p
         INNER JOIN 
             AIR_TRAFFIC_CONTROLLERS atc
         ON 
         (
-            e.Emp_Code = p.Emp_Code
-            AND e.Emp_Code = p.Emp_Code
-            AND e.Emp_Code = atc.Emp_Code
+            e.Emp_Code = atc.Emp_Code
         )"
     );
     if( !$controllers ) {
