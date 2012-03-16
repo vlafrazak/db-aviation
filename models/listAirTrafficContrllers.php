f1f6a1924f1bcb4ec39a0c52ec2e2ff40262db3f
Gregory Liras <gregliras@gmail.com>
2012-03-17 01:14:34 +0200

    edit in AtC

--- models/listAirTrafficContrllers.php
+++ models/listAirTrafficContrllers.php
@@ -3,15 +3,12 @@
 
     $controllers = query_array( 
         "SELECT 
-            e . * , atc.Last_Exam_Date, atc.Exam_Result
+            *
         FROM 
-            EMPLOYEES e
-        INNER JOIN 
-            AIR_TRAFFIC_CONTROLLERS atc
-        ON 
-        (
-            e.Emp_Code = atc.Emp_Code
-        )"
+            EMPLOYEES e. AIR_TRAFFIC_CONTROLLERS atc
+        WHERE
+            e.Emp_Code = atc.Emp_Code"
+            
     );
     if( !$controllers ) {
         die( mysql_error() );
