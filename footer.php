f31c8f13b53097cfbb2edfbeb21faf58e8eb5b5c
ValiaV <you@example.com>
2012-03-17 18:13:08 +0200

    footer_3

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
-</div>
-  <div class="clearfooter"></div>
-
-  <div id="footer"><p>Credits: 
+  
+        <div id="footer"><p>Credits: 
 		<a href="mailto:lydiazak90@gmail.com">  Lydia Zak </a> ,
 		<a href="mailto:iren.vlsi@gmail.com">  Iren Vlassi</a> , 	
 		<a href="mailto:valia.fragiad@gmail.com">  Valia Fr</a>.
-  	</div><!-- End of container-->	
-	</body>
+  	</div><!-- End of footer-->	
+	</div><!-- End of container-->	
 </html>
 
--- header.php
+++ header.php
@@ -3,44 +3,42 @@ Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-
 strict.dtd">
 
 <html>
-	<head>
-		<title>AIRPORT EL.VENIZELOS</title>
-		<meta
-                	http-equiv="Content-Type"
-                	content = "text/html; charset=utf-8;"
-        	/>
-		<link
-        		rel = "stylesheet"
-        		type = "text/css"
-			href = "/~db/db-aviation/css/style.css" />
-		<link rel = "shotcut icon" href= "/~db/db-aviation/images/maurouli.jpg"/>
-	</head>
-    <body>
-	<!--#include virtual="/style/csbanner.ssi"-->
-	<h1 id = "top">     ΑΕΡΟΔΡΟΜΙΟ   ΕΛΕΥΘΕΡΙΟΣ   ΒΕΝΙΖΕΛΟΣ
-		</h1>
-<?php
-    $myset = include "settings-local.php";
-?>
+	<div id = "container">
+		<head>
+			<title>AIRPORT EL.VENIZELOS</title>
+			<meta
+                		http-equiv="Content-Type"
+                		content = "text/html; charset=utf-8;"
+        		/>
+			<link
+        			rel = "stylesheet"
+        			type = "text/css"
+				href = "style.css" />
+			<link rel = "shotcut icon" href= "/~db/db-aviation/images/maurouli.jpg"/>
+		</head>
+    	<body>
+		<h1 id = "top">     ΑΕΡΟΔΡΟΜΙΟ   ΕΛΕΥΘΕΡΙΟΣ   ΒΕΝΙΖΕΛΟΣ</h1>
+		<?php
+    			$myset = include "settings-local.php";
+		?>
 
-	<div id="menu">
-		<ul>
-		<li class="mainMenu"><!--<img 
-		            src = "/~db/db-aviation/images/maurouli.jpg"
-                	alt= "The db is not a lie..." 
-                	width="50" 
-                	hight="50" /> -->
-		<a href="/~db/db-aviation">Αρχική Σελίδα</a></li>
-        <li class="mainMenu" ><a href="/~db/db-aviation/views/listEmployees/">Εργαζόμενοι</a>
-        <li><a href="/~db/db-aviation/views/listTechnicians/">Τεχνικοί</a></li>
-        <li><a href="/~db/db-aviation/views/listAirTrafficControllers/">Χειριστές Εναέριας Κυκλοφορίας</a></li>
-        </li>
-		<li class="mainMenu" ><a href="/~db/db-aviation/views/listAircraftFleet/">Βάση Αεροσκαφών</a></li>
-		<!--<li><a href="/~db/db-aviation/views/listAircraftModels/">Μοντέλα Αεροσκαφών</a></li>
-		<li><a href="/~db/db-aviation/views/listChecks/">Έλεγχοι</a></li>-->
-		</ul>
+		<div id="menu">
+			<ul>
+			<li class="mainMenu"><!--<img 
+			            src = "/~db/db-aviation/images/maurouli.jpg"
+                		alt= "The db is not a lie..." 
+                		width="50" 
+                		hight="50" /> -->
+			<a href="/~db/db-aviation">Αρχική Σελίδα</a></li>
+        		<li class="mainMenu" ><a href="/~db/db-aviation/views/listEmployees/">Εργαζόμενοι</a>
+        		<li><a href="/~db/db-aviation/views/listTechnicians/">Τεχνικοί</a></li>
+      		  	<li><a href="/~db/db-aviation/views/listAirTrafficControllers/">Χειριστές Εναέριας Κυκλοφορίας</a></li>
+        		</li>
+			<li class="mainMenu" ><a href="/~db/db-aviation/views/listAircraftFleet/">Βάση Αεροσκαφών</a></li>
+			<!--<li><a href="/~db/db-aviation/views/listAircraftModels/">Μοντέλα Αεροσκαφών</a></li>
+			<li><a href="/~db/db-aviation/views/listChecks/">Έλεγχοι</a></li>-->
+			</ul>
 		
-	</div>
-	<div id = "content"
-
+		</div>
+	</body>
 
