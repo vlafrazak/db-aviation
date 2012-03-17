8f00347e2d40d6e3b42254ed08f8cea7bf209862
Gregory Liras <gregliras@gmail.com>
2012-03-18 00:41:01 +0200

    isexpert seems complete, refactored emails,phones

--- views/listIsExpertIn.php
+++ views/listIsExpertIn.php
@@ -6,11 +6,11 @@
 <table id="playlist" class = "content">
 <tr class="even">
 <th>Emp Code</th>
+<th>Experience</th>
 <th>First Name</th>
 <th>Surname</th>
 <th>E-mail</th>
 <th>Phone</th>
-<th>Experience</th>
 </tr>
 
 <?php
@@ -19,9 +19,25 @@
     foreach($experts as $expert) {
         echo "\n<tr>";
         echo "<td>" .  $expert[ 'Emp_Code' ] . "</td>";
+        echo "<td>" .  $expert[ 'Degree_of_Experience' ] . "</td>";
         echo "<td>" .  $expert[ 'First_Name' ] . "</td>";
         echo "<td>" .  $expert[ 'Surname' ] . "</td>";
-        echo "<td>" .  $expert[ 'Degree_of_Experience' ] . "</td>";
+        echo "<td>";
+        echo "<ul>";
+        foreach( $tech['mails'] as $mail ) {
+            echo "<li><a href='mailto:" .  $mail[0]  . "'>" . $mail[0] . "</a></li>";
+        }
+        echo "</ul>";
+        echo "</td>";
+        
+        echo "<td>";
+        echo "<ul>";
+        foreach( $tech['phones'] as $phone ) {
+            echo "<li> $phone[0] </li>";
+        }
+        echo "</ul>";
+        echo "</td>";
+       
         echo "</tr>";
     }
 ?>
