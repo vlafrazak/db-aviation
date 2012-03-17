c797e02a712ad8d6e73887b0c639f864f996afbf
user <valia@ubuntu.(none)>
2012-03-17 15:25:02 +0200

    test footer

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
 
-	<div id = "footer">
-		  Credits: Lydia Zak, Iren Vlassi, Valia Fr 
-	</div>
+  <div class="clearfooter"></div>
 
-</body>
+  <div id="footer"><p>Credits: 
+		<a href="mailto:lydiazak90@gmail.com">  Lydia Zak </a> ,
+		<a href="mailto:iren.vlsi@gmail.com">  Iren Vlassi</a> , 	
+		<a href="mailto:valia.fragiad@gmail.com">  Valia Fr</a>.
+  	</div><!-- End of container-->	
+	</body>
 </html>
 
