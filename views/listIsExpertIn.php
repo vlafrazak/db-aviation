541c36bd944715e7a12f8e5f97b581e2865141fb
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-18 00:07:02 +0200

    corrected typo

--- views/listIsExpertIn.php
+++ views/listIsExpertIn.php
@@ -2,6 +2,6 @@
     include "../models/listIsExpertIn.php";
     $experts = isExpertIn( $_GET[ 'Model_Code' ] );
     foreach($experts as $expert) {
-        echo $expert
+        echo $expert;
     }
 ?>
