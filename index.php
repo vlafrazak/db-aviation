edd006f9bfab0cc0449c7b349522f78fa53a4bf7
ValiaV <you@example.com>
2012-03-17 20:59:08 +0200

    arxikh- eikona mono

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
     include 'header.php';
 ?>
 
+<img id = "eikona"
+		src = "/~db/db-aviation/images/first.jpg"
+                alt= "The db is not a lie..." 
+             	width="50" 
+                hight="50"
+		align = center
+	/>
+
 <?php
     include 'models/connect.php';
 ?>
