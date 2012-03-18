5905c9d155dad6004fab14d31cacbdeba1331c18
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-19 00:55:22 +0200

    technicians gamhsane

--- models/insertTechnicians.php
+++ models/insertTechnicians.php
@@ -9,6 +9,17 @@
        $_POST[Emp_Code],
        $_POST[Rank]
    )");
+  query(
+   "INSERT INTO IS_EXPERT_IN (
+       Emp_Code,
+       Model_Code,
+       Degree_of_Experience
+   ) 
+   VALUES (
+       $_POST[Emp_Code],
+       $_POST[Model_Code],
+       $_POST[Degree_of_Experience]
+   )");
 
      
     $numargs = func_num_args();
