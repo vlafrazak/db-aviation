cc12207537f0c68f896136abe8db54791e6c2393
Gregory Liras <gregliras@gmail.com>
2012-03-18 20:18:12 +0200

    should be functional now

--- models/database.php
+++ models/database.php
@@ -1,40 +1,48 @@
 <?php
 include "connect.php";
-function query($query) {
-    connect();
-    $result = mysql_query($query) or die(mysql_error());
-    if ( !$result ) {
-        die( mysql_error() );
+if( !function_exists('query'))
+{
+    function query($query) {
+        connect();
+        $result = mysql_query($query) or die(mysql_error());
+        if ( !$result ) {
+            die( mysql_error() );
+        }
+        disconnect();
     }
-    disconnect();
+}
+if( !function_exists('query_array'))
+{
+    function query_array($query) {
+        connect();
+        $result = mysql_query($query) or die(mysql_error());
+        if ( !$result ) {
+            die( mysql_error() );	
+        }
+
+        while($row = mysql_fetch_array($result))
+        {
+            $answer[] = $row;
+        } 
+        //mysql_free_result($result);
+        disconnect();
+        return $answer;
+    }
 }
 
-function query_array($query) {
-    connect();
-    $result = mysql_query($query) or die(mysql_error());
-    if ( !$result ) {
-        die( mysql_error() );	
+if( !function_exists('query_row'))
+{
+    function query_row($query) {
+        connect();
+        $result = mysql_query($query) or die(mysql_error());
+        if ( !$result ) {
+            die( mysql_error() );
+        }
+
+        $row = mysql_fetch_array($result);
+        //mysql_free_result($result);
+        disconnect();
+        return $answer;
     }
-
-    while($row = mysql_fetch_array($result))
-    {
-        $answer[] = $row;
-    } 
-    //mysql_free_result($result);
-    disconnect();
-    return $answer;
-}
-
-function query_row($query) {
-    connect();
-    $result = mysql_query($query) or die(mysql_error());
-    if ( !$result ) {
-        die( mysql_error() );
-    }
-
-    $row = mysql_fetch_array($result);
-    //mysql_free_result($result);
-    disconnect();
-    return $answer;
 }
 ?>
