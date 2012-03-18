d70cb03c3eab202b5a2fde297ddc170d03db8416
ValiaV <valia.fragiad@gmail.com>
2012-03-18 18:54:13 +0200

    koumpia submit 5

--- views/updateAircraftModels.php
+++ views/updateAircraftModels.php
@@ -10,7 +10,7 @@
     <tr class="odd"><td>Model_Code:</td><td> <input type="int" readonly="readonly" name="Model_Code" value="<?php echo rtrim($_POST['Model_Code'],'/'); ?>"/></td></tr>
     <tr class="even"><td>Model_Name:</td><td> <input type="text" name="Model_Name" value="<?php echo rtrim($_POST['Model_Name'],'/'); ?>"/></td></tr>
     <tr class="odd"><td>Capacity:</td><td> <input type="int" name="Capacity" value="<?php echo rtrim($_POST['Capacity'],'/'); ?>"/></td></tr>
-    <tr><td>Weight:</td><td> <input type="int" name="Weight" value="<?php echo rtrim($_POST['Weight'],'/'); ?>"/></td></tr>
+    <tr class = "even"><td>Weight:</td><td> <input type="int" name="Weight" value="<?php echo rtrim($_POST['Weight'],'/'); ?>"/></td></tr>
     <tr class="odd"><td>Manufacturer:</td><td> <input type="text" name="Manufacturer" value="<?php echo rtrim($_POST['Manufacturer'],'/'); ?>"/></td></tr>
     </table>
 
