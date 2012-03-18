0a83120b5a7e20639045c18925124d5a35138c93
Gregory Liras <gregliras@gmail.com>
2012-03-18 02:45:28 +0200

    added views for email

---
+++ views/createEmail.php
@@ -0,0 +1,23 @@
+<?php
+    include "../header.php"
+?>
+
+<h2>Insert an e-mail for
+<?php
+    echo $_GET[ 'First_Name' ] . " " . $_GET[ 'Surname' ];
+?>
+</h2>
+
+<form action='/~db/db-aviation/models/insertEmail.php' method='POST'>
+    <table id="playlist" class ="input form">
+    <tr class="odd"><td>First Name:</td><td> <input type="text" name="First_Name" value="<?php echo $_GET[ 'First_Name' ]?>"/></td></tr>
+    <tr class="even"><td>Surname:</td><td> <input type="text" name="Surname" value="<?php echo $_GET[ 'Surname' ]?>"/></td></tr>
+    <tr class="odd"><td>E-mail:</td><td> <input type="text" name="E_mail" /></td></tr>
+    </table>
+    <input type="hidden" name="Emp_Code" value="<?php echo $_GET[ 'Emp_Code' ]}?>"/></td></tr>
+    <input type='submit' value='Submit' title='Προσθήκη'/>                                                   
+</form>
+
+<?php
+    include "../footer.php"
+?>
