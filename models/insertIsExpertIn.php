b28ba5628a1d64ad6de7b7d2f0261354f4e036f6
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-18 23:45:10 +0200

    expert

---
+++ models/insertIsExpertIn.php
@@ -0,0 +1,20 @@
+<?php  
+  include 'database.php';
+  query(
+      "INSERT INTO IS_EXPERT_IN (
+        Emp_Code,
+        Model_Code,
+        Degree_of_Experience
+    )
+    VALUES (
+        $_POST[Emp_Code],
+        $_POST[Model_Code],
+        $_POST[Degree_of_Experience]
+    )");
+  
+    $numargs = func_num_args();
+    echo "Number of arguments: $numargs\n";
+    Header ('Location: /~db/db-aviation/views/listEmployees.php');
+?>
+
+
