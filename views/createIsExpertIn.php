4fd9d20d93030d93e42b44e69728e9908f43b042
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-19 00:11:01 +0200

    expert ki allo

---
+++ views/createIsExpertIn.php
@@ -0,0 +1,35 @@
+<?php
+    include "../header.php";
+    $models = include "../models/listAircraftModels.php";
+?>
+
+<h2>Add an Expertise for
+<?php
+    echo $_GET[ 'First_Name' ] . " " . $_GET[ 'Surname' ];
+?>
+</h2>
+
+<form action='/~db/db-aviation/models/insertIsExpertIn.php' method='POST'>
+    <table id="playlist" class ="input form">
+    <tr class="odd"><td>Emp Code:</td><td> <input type="int" readonly="readonly" name="Emp_Code" value="<?php echo $_GET[ 'Emp_Code' ]?>"/></td></tr>
+    <tr class="even"><td>Model Code:</td><td> 
+        <select type="int" name="Model_Code">
+            <?php
+                foreach( $models as $model) {
+                        echo "<option value='".$model[ 'Model_Code' ]."'>".$model[ 'Model_Code' ]." - ".$model[ 'Model_Name' ]."</option>";
+                }
+            ?>
+        </select>
+    </td>
+    </tr>
+    <tr class="odd"><td>Degree of Experience:</td><td> <input type="int" name="Degree_of_Experience" /></td></tr>
+    </table>
+    <input type="hidden" name="Emp_Code" value="<?php echo $_GET[ 'Emp_Code' ]?>"/></td></tr>
+    <input type='submit' value='Submit' title='Προσθήκη'/>                                                   
+</form>
+
+<?php
+    include "../footer.php"
+?>
+
+
