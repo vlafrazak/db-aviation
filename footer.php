acfe9fbd770fc4587bafeb405f5ba322c3ec22d0
ValiaV <valia.fragiad@gmail.com>
2012-03-19 15:34:12 +0200

    allages css

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
         <div id="footer"><p>Credits: 
 		<a href="mailto:lydiazak90@gmail.com">  Lydia Zak </a> ,
-		<a href="mailto:iren.vlsi@gmail.com">  Iren Vlassi</a> , 	
+		<a href="mailto:iren.vlsi@gmail.com">  Iren Vlsi</a> , 	
 		<a href="mailto:valia.fragiad@gmail.com">  Valia Fragkia</a>.
   	</div><!-- End of footer-->	
 	</body> <!--end of title and menu-->
