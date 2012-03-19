0fc44b544419e458c7563ccc26abce44018968cf
ValiaV <valia.fragiad@gmail.com>
2012-03-19 17:13:07 +0200

    shuffle! :Credits to Vasilhs

--- footer.php
+++ footer.php
@@ -1,8 +1,27 @@
+<!--shuffle names : Credits to Vasilhs-->
 
         <div id="footer"><p>Credits: 
-		<a href="mailto:lydiazak90@gmail.com">  Lydia Zak </a> ,
-		<a href="mailto:iren.vlsi@gmail.com">  Iren Vlsi</a> , 	
-		<a href="mailto:valia.fragiad@gmail.com">  Valia Fragkia</a>.
+
+<?php
+            $contributors = array(
+                'Lydia Zak' => 'lydiazak90@gmail.com',
+                'Iren Vlsi' =>  'iren.vlsi@gmail.com',
+                'Valia Fragkia' => 'valia.fragiad@gmail.com'
+            );
+            shuffle( $contributors );
+            foreach ( $contributors as $contributor => $url ) {
+                    if ($url == 'lydiazak90@gmail.com'){
+                        echo '<a href="mailto:' . $url . '">' . 'Lydia Zak </a>';
+                    }
+                    else if ($url == 'iren.vlsi@gmail.com'){
+                        echo '<a href="mailto:' . $url . '">' . 'Iren Vlsi </a>';
+                    }
+                    else {
+                        echo '<a href="mailto:' . $url . '">' . 'Valia Fragk </a>';
+                    }
+            }
+            ?>
+
   	</div><!-- End of footer-->	
 	</body> <!--end of title and menu-->
 </html>
