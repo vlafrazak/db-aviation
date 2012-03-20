5154aaf18693a1c70f3a9ebccfa99e31162597b8
iren <iren.vlsi@gmai.com>
2012-03-20 05:52:40 +0200

    olika pali

--- views/listTechnicians.php
+++ views/listTechnicians.php
@@ -5,7 +5,19 @@
 <h2>Technicians</h2>
 <table id="playlist" class = "content">
 <tr class="even">
-<th>Code</th>
+<th>Emp Code</th>
+<th>SSN</th>
+<th>UMN</th>
+<th>First Name</th>
+<th>Surname</th>
+<th>Street Name</th>
+<th>Street Number</th>
+<th>Postal Code</th>
+<th>Year of Birth</th>
+<th>Salary</th>
+<th>E-mail</th>
+<th>Phone</th>
+<th>Expertise</th>
 <th>Rank</th>
 <th></th>
 <th></th>
@@ -18,8 +30,51 @@
     foreach( $techs as $tech ) {
         echo "\n<tr>";
         
-        echo "<td>" .  $tech[ 'Code' ] . "</td>";
-            echo "<td>" .  $tech[ 'Rank' ] . "</td>";
+        echo "<td>" .  $tech[ 'Emp_Code' ] . "</td>";
+        echo "<td>" .  $tech[ 'SSN' ] . "</td>";
+        echo "<td>" .  $tech[ 'Union_Membership_Number' ] . "</td>";
+        echo "<td>" .  $tech[ 'First_Name' ] . "</td>";
+        echo "<td>" .  $tech[ 'Surname' ] . "</td>";
+        echo "<td>" .  $tech[ 'Street_Name' ] . "</td>";
+        echo "<td>" .  $tech[ 'Street_Number' ] . "</td>";
+        echo "<td>" .  $tech[ 'Postal_Code' ] . "</td>";
+        echo "<td>" .  $tech[ 'Year_of_Birth' ] . "</td>";
+        echo "<td>" .  $tech[ 'Salary' ] . "</td>";
+
+        echo "<td class='emails'>";
+        echo "<table class='noframe'><tr><td class='noframe'></td><td class='noframe'><a class='button' href='/~db/db-aviation/views/createEmail.php?Emp_Code=" . $tech[ 'Emp_Code' ] .
+                                                            "&First_Name=" . $tech[ 'First_Name' ] .
+                                                            "&Surname=" . $tech[ 'Surname' ] .
+                                                            "'>+</a></td></tr>";
+        foreach( $tech['mails'] as $mail ) {
+            echo "<tr><td class='noframe'><a class='emails' href='mailto:" .  $mail[0]  . "'>" . $mail[0] . "</a></td><td class='noframe'><a class='button' href='/~db/db-aviation/models/deleteEmail.php?E_mail=". $mail[0] ."'>x</a></td></tr>";
+        }
+        echo "</table>";
+        echo "</td>";
+        
+        echo "<td class='phones'>";
+        echo "<table class='noframe'><tr><td class='noframe'></td><td class='noframe'><a class='button' href='/~db/db-aviation/views/createPhone.php?Emp_Code=" . $tech[ 'Emp_Code' ] .
+                                                            "&First_Name=" . $tech[ 'First_Name' ] .
+                                                            "&Surname=" . $tech[ 'Surname' ] .
+                                                            "'>+</a></td></tr>";
+        foreach( $tech['phones'] as $phone ) {
+            echo "<tr><td class='noframe'> $phone[0] </td> <td class='noframe'><a class='button' href='/~db/db-aviation/models/deletePhone.php?Phone=". $phone[0] ."'>x</a></td></tr>";
+        }
+        echo "</table>";
+        echo "</td>";
+        
+        echo "<td class='phones'>";
+        echo "<table class='noframe'><tr><td><td class='noframe'></td></td><td class='noframe'><a class='button' href='/~db/db-aviation/views/createIsExpertIn.php?Emp_Code=" . $tech[ 'Emp_Code' ] .
+                                                            "&First_Name=" . $tech[ 'First_Name' ] .
+                                                            "&Surname=" . $tech[ 'Surname' ] .
+                                                            "'>+</a></td></tr>";
+        foreach( $tech['expertise'] as $expertise ) {
+            echo "<tr><td class='noframe'>". $expertise[2]. "<td class='noframe'> $expertise[1] </td></td> <td class='noframe'><a class='button' href='/~db/db-aviation/models/deleteIsExpertIn.php?Emp_Code=". $tech[ 'Emp_Code' ] ."&Model_Code=".$expertise[0]."'>x</a></td></tr>";
+        }
+        echo "</table>";
+        echo "</td>";
+  
+        echo "<td>" .  $tech[ 'Rank' ] . "</td>";
         
         echo "<td><form action='/~db/db-aviation/views/updateTechnicians.php' method='post'>";
         echo "<input type='hidden' name='Emp_Code' value=" . $tech[ 'Emp_Code' ] . "/>";
