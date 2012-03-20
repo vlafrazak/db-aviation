e55ebac1297c34ec225ef059171c84bfa55e184a
Iren Vlsi <iren.vlsi@gmail.com>
2012-03-20 05:58:35 +0200

    den to thelw

--- models/listTechnicians.php
+++ models/listTechnicians.php
@@ -1,47 +1,13 @@
 <?php  
     include 'database.php';
-    include 'listEmails.php';
-    include 'listPhones.php';
 
-    $techs = query_array( 
-        "SELECT e.*, t.Rank
+    $techs = query( 
+        "SELECT *
         FROM 
-            EMPLOYEES e
-        INNER JOIN 
-            TECHNICIANS t
-        ON 
-            e.Emp_Code = t.Emp_Code"
-    );
+            TECHNICIANS
+    ");
     if( !$techs ) {
         die( mysql_error() );
     }
 
-    $answer = array();
-    foreach( $techs as $tech) {
-        $tech[ 'mails' ] = query_array( 
-            "SELECT e.E_mail
-                FROM
-            E_MAILS e
-                WHERE
-                e.Emp_Code = " . $tech[ 'Emp_Code' ] );
-        $tech[ 'phones' ] = query_array(
-                "SELECT p.Phone_number
-                FROM
-                    PHONES p
-                WHERE
-                    p.Emp_Code = " . $tech[ 'Emp_Code' ] );
-        $tech[ 'expertise' ] = query_array(
-                "SELECT ex.Model_Code,ex.Degree_of_Experience,m.Model_Name
-                FROM
-                    IS_EXPERT_IN ex
-                INNER JOIN
-                    AIRCRAFT_MODEL m
-                ON
-                    ex.Model_Code = m.Model_Code
-                WHERE
-                    ex.Emp_Code = " . $tech[ 'Emp_Code' ] );
-        
-        $answer[] = $tech;
-    }
-    return $answer;    
 ?>
